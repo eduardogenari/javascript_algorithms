cb9b42ea8923dea5a8990972275a481260d7fd61
Eduardo Genari <67627261+eduardogenari@users.noreply.github.com>
2021-12-06 17:46:56 +0100

    Add files via upload

---
+++ 201019/main.js
@@ -0,0 +1,5 @@
+function printarMensaje(){
+    let mensaje = document.getElementById("cajaTexto").value;//Recoger valor de un input text
+
+    document.getElementById("mostrarDatos").innerHTML = mensaje;//Dar valor por ejemplo a un párrafo
+}
